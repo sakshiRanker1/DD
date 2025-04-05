b15a0b201d0aea5820918c6d3256598caecef5a4
Sakshi Jha <129147399+sakshiRanker1@users.noreply.github.com>
2025-04-05 23:09:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    // function createHearts() {
+    //   for (let i = 0; i < 50; i++) {
+    //     const heart = document.createElement('div');
+    //     heart.classList.add('heart');
+    //     heart.style.left = Math.random() * window.innerWidth + 'px';
+    //     heart.style.top = Math.random() * window.innerHeight + 'px';
+    //     document.body.appendChild(heart);
+
+    //     setTimeout(() => {
+    //       heart.remove();
+    //     }, 4000);
+    //   }
+    // }
+
+    // function toggleMessage() {
+    //   const checkbox = document.getElementById('showMessage');
+    //   const message = document.getElementById('message');
+    //   if (checkbox.checked) {
+    //     message.style.display = 'block';
+    //   } else {
+    //     message.style.display = 'none';
+    //   }
+    // }
+  
